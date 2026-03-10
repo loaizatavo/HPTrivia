56def72391595aeb88558047dab38bfe3d4e4967
Gustavo Loaiza Robles <loaizatavo@gmail.com>
2026-03-10 09:57:20 -0600

    Initial Commit

---
+++ HPTrivia/HPTriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HPTriviaApp.swift
+//  HPTrivia
+//
+//  Created by Gustavo Loaiza Robles on 10/3/26.
+//
+
+import SwiftUI
+
+@main
+struct HPTriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
