7d181e7eba9f652b576ab83152861f036e25010d
Gustavo Loaiza Robles <loaizatavo@gmail.com>
2026-03-18 20:45:28 -0600

    add some animation to the title of the ContentView

--- HPTrivia/Views/ContentView.swift
+++ HPTrivia/Views/ContentView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animateViewIn = false
+    
     var body: some View {
         GeometryReader { geo in
             ZStack {
@@ -21,10 +23,41 @@ struct ContentView: View {
                     } animation: { _ in
                             .linear(duration: 60)
                     }
+                
+                VStack{
+                    Group {
+                        if animateViewIn {
+                            VStack {
+                                Image(systemName: "bolt.fill")
+                                    .imageScale(.large)
+                                    .font(.largeTitle)
+                                
+                                Text("HP")
+                                    .font(.custom("PartyLetPlain", size: 70))
+                                    .padding(.bottom, -50)
+                                
+                                Text("Trivia")
+                                    .font(.custom("PartyLetPlain", size: 60))
+                                
+                            }
+                            .padding(.top, 70)
+                            .transition(.move(edge: .top))
+                        }
+                    }
+                    .animation(.easeOut(duration: 0.7).delay(2), value: animateViewIn)
+                    
+                    Spacer()
+                }
             }
             .frame(width: geo.size.width, height: geo.size.height)
         }
         .ignoresSafeArea()
+        .onAppear {
+            //withAnimation {
+                animateViewIn = true
+            //}
+        }
+    
     }
 }
 
